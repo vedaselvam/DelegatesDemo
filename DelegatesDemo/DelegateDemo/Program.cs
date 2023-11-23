bd560266ce07f911dbc69393a68abadf05bb16ef
veda <vedaselvam@hotmail.com>
2023-11-23 14:32:18 +0800

    add delegate demo

---
+++ DelegatesDemo/DelegateDemo/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using DelegateLib;
+
+Console.WriteLine("Hello, World!");
+
+AppSecurity.RunUsingAppCredentials(delegate ()
+{
+    var ctMethod = System.Reflection.MethodBase.GetCurrentMethod();
+    Console.WriteLine($"Output from {ctMethod.DeclaringType}.{ctMethod.Name}");
+});
---
+++ DelegatesDemo/DelegateLib/AppSecurity.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace DelegateLib
+{
+    public static class AppSecurity
+    {
+        public static void RunUsingAppCredentials(Delegate d)
+        {
+            d.DynamicInvoke();
+            var ctMethod = System.Reflection.MethodBase.GetCurrentMethod();
+            Console.WriteLine($"Output from {ctMethod.DeclaringType}.{ctMethod.Name}");
+        }
+    }
+}
